b903a6ff4799b8808446fc64d12ac30cdff575e7
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2021-09-20 20:04:47 -0500

    agregar nueva funcion al proyecto

--- __test__/challenge.test.js
+++ __test__/challenge.test.js
@@ -1,4 +1,4 @@
-const {suma} = require("../index")
+const {suma, resta} = require("../index")
 
 test("Deberia pasar 8 al pasar como argumentos 6  y 2 ", ()=>{
   let a = 6;
@@ -7,4 +7,12 @@ test("Deberia pasar 8 al pasar como argumentos 6  y 2 ", ()=>{
   let result = suma(a,b);
 
   expect(result).toBe(8)
+})
+test("Deberia pasar 6 al pasar como argumentos 8  y 2 ", ()=>{
+  let a = 8;
+  let b = 2;
+
+  let result = resta(a,b);
+
+  expect(result).toBe(6)
 })
